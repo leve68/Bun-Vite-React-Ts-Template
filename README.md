30a1cab2a02b56766c404f87cc2106b14c696b55
leve68 <hanstom000608@gmail.com>
2025-03-17 23:34:25 +0900

    chore: project init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Portfolio
+Bun Vite React TS
+
+eslint prettier tailwind react-router-dom shadcn/ui lucide-react framer-motion
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import macrosPlugin from "vite-plugin-babel-macros";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [macrosPlugin(), react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  define: {
+    global: {},
+  },
+});

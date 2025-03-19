7263f30bdf4358fcdde3945bd23d3fc064e843d3
leve68 <hanstom000608@gmail.com>
2025-03-20 00:04:26 +0900

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import { Button } from "@/components/ui/button";
-
 function App() {
-  return <Button>Hello World</Button>;
+  return (
+    <div className="min-h-screen w-screen bg-gray-100 p-40">
+      <div className="bg-pink-100">
+        <div>Title</div>
+        <div>description</div>
+      </div>
+      <div className="flex flex-col gap-32">
+        <div className="flex justify-between h-fit">
+          <div className="flex flex-col">
+            <div>title</div>
+            <div>description</div>
+            <div>description</div>
+            <a className="mt-auto underline">link</a>
+          </div>
+          <img src="" alt="img1" className="bg-black w-160 h-80" />
+        </div>
+        <div className="flex justify-between h-fit">
+          <div className="flex flex-col">
+            <div>title</div>
+            <div>description</div>
+            <div>description</div>
+            <a className="mt-auto underline">link</a>
+          </div>
+          <img src="" alt="img1" className="bg-black w-160 h-80" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
